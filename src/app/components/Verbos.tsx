4fedd25e188f8df2226fa139aa4bf4d9c752d62a
Edson Santos <129888422+Edsonsantos0840@users.noreply.github.com>
2024-10-29 22:18:03 -0300

    Initial commit from Create Next App

---
+++ src/app/components/Verbos.tsx
@@ -0,0 +1,23 @@
+export const verbos = [
+    "am",
+    "are",
+    "is",
+    "speak",
+    "have",
+    "help",
+    "understand",
+    "repeat",
+    "say",
+    "know",
+    "like",
+    "take",
+    "pay",
+    "need",
+    "go",
+    "show",
+    "find",
+    "wait",
+    "open",
+    "close",
+    "see"
+];
